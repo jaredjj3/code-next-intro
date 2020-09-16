b9dcc3f459c8c2c90e7c4c5119ad49b48d0bb73a
jaredjj3 <jared@jaredjohnson.dev>
2020-09-16 09:28:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# code-next-intro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/code-next-intro)
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const jsApp = document.getElementById('js-app');
+jsApp.innerHTML = '<h1>Hello from the DOM!</h1>';
+console.log('Hello from the console!');
